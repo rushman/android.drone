26bfdc34d40efdf72c38c7511091ebeee7535667
acid <acidoverflow@gmail.com>
2011-06-20 15:10:06 +0200

    Change-Id: I0180487c74f061f9faa1780fac2726cdacea4147

--- src/de/leuphana/android/drone/Drone.java
+++ src/de/leuphana/android/drone/Drone.java
@@ -6,7 +6,6 @@ import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.TextView;
-import de.leuphana.android.drone.connector.ConnectToDrone;
 import de.leuphana.android.drone.connector.DroneCommands;
 import de.leuphana.android.hello.world.R;
 
@@ -27,16 +26,16 @@ public class Drone extends Activity {
 		// R = LayoutClass from res
 		this.setContentView(R.layout.main);
 
-		ConnectToDrone connection = new ConnectToDrone("192.168.1.1");
+		// ConnectToDrone connection = new ConnectToDrone("192.168.1.1");
 
-		this.control = new DroneCommands(connection);
+		// this.control = new DroneCommands(connection);
 
 	}
 
 	// Method for starting the drone and sending Information to View.
 	public void startUp(View view) {
 
-		this.control.start();
+		// this.control.start();
 
 		TextView textView = (TextView) this.findViewById(R.id.helloTextView);
 
@@ -47,7 +46,7 @@ public class Drone extends Activity {
 	// Method for landing the drone and sending Information to View.
 	public void getDown(View view) {
 
-		this.control.land();
+		// this.control.land();
 
 		TextView textView = (TextView) this.findViewById(R.id.helloTextView);
 
@@ -58,7 +57,7 @@ public class Drone extends Activity {
 	// Method for turning the drone left and sending Information to View.
 	public void turnLeft(View view) {
 
-		this.control.turnHorz(0.2f);
+		// this.control.turnHorz(0.2f);
 
 		TextView textView = (TextView) this.findViewById(R.id.helloTextView);
 
@@ -69,7 +68,7 @@ public class Drone extends Activity {
 	// Method for turning the drone right and sending Information to View.
 	public void turnRight(View view) {
 
-		this.control.turnHorz(-0.2f);
+		// this.control.turnHorz(-0.2f);
 
 		TextView textView = (TextView) this.findViewById(R.id.helloTextView);
 
--- src/de/leuphana/android/drone/blkubbb.java
+++
@@ -1,5 +0,0 @@
-package de.leuphana.android.drone;
-
-public class blkubbb {
-
-}
